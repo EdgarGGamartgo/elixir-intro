79e6207572abda3d928fa3805ebae6b991ad6638
EdgarGGamartgo <edgarggamartgo@gmail.com>
2022-12-19 20:39:03 -0600

    add cards module

---
+++ cards/lib/cards.ex
@@ -0,0 +1,29 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def konnichiwa do
+    "mata ne"
+  end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+  end
+end
